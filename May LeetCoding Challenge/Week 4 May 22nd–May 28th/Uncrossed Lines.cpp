a2c8e231fb61774b395fb03a2cdc4846178f05be
suraj kumar oraon <surajcroaxx00097@gmail.com>
2020-05-25 17:10:49 +0530

    Create Uncrossed Lines.cpp

---
+++ May LeetCoding Challenge/Week 4 May 22nd–May 28th/Uncrossed Lines.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxUncrossedLines(vector<int>& A, vector<int>& B) {
+        int m=A.size();
+        int n=B.size();
+        vector<vector<int>>v(m+1,vector<int>(n+1,0));
+        for(int i=1;i<=m;i++)
+            for(int j=1;j<=n;j++)
+            {
+                if(A[i-1]==B[j-1]) 
+                    v[i][j]=v[i-1][j-1]+1;
+                else v[i][j]=max(v[i-1][j],v[i][j-1]);
+            }
+        return v[m][n];
+    }
+};
