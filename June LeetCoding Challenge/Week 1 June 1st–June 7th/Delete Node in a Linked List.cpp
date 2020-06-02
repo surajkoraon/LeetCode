a675f55402b3ba500e168918d0301c010e84cb03
suraj kumar oraon <surajcroaxx00097@gmail.com>
2020-06-02 22:11:51 +0530

    Update Delete Node in a Linked List.cpp

--- June LeetCoding Challenge/Week 1 June 1st–June 7th/Delete Node in a Linked List.cpp
+++ June LeetCoding Challenge/Week 1 June 1st–June 7th/Delete Node in a Linked List.cpp
@@ -6,6 +6,26 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        while(1)
+        {
+            node->val=node->next->val;
+            if(node->next->next)
+                node=node->next;
+            else
+            {
+                node->next=NULL;
+                break;
+            }
+        }
+    }
+};
+
+//Another solution
+
 class Solution {
 public:
     void deleteNode(ListNode* node) {
