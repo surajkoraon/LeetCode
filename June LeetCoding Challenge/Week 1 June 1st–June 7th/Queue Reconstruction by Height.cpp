65e4234b286220fd2cacebe7ab06ed4bdc1972cd
suraj kumar oraon <surajcroaxx00097@gmail.com>
2020-06-06 22:39:58 +0530

    Create Queue Reconstruction by Height.cpp

---
+++ June LeetCoding Challenge/Week 1 June 1st–June 7th/Queue Reconstruction by Height.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
+        sort(people.begin(),people.end(),[](vector<int> a,vector<int> b)
+             {
+                 if(a[0]==b[0])
+                 {
+                     return a[1]<b[1];
+                 }
+                 return a[0]<b[0];
+             });
+        vector<vector<int>> v(people.size(),vector<int>(2,-1));
+        for(int i=0;i<people.size();i++)
+        {
+            int c=people[i][1];
+            for(int j=0;j<people.size();j++)
+            {
+                if(c==0 && v[j][0]==-1)
+                {
+                    c=j;
+                    break;
+                }
+                if(v[j][0]==-1 || v[j][0]>=people[i][0])
+                    c--;
+            }
+            v[c][0]=people[i][0];
+            v[c][1]=people[i][1];
+        }
+        return v;
+    }
+};
