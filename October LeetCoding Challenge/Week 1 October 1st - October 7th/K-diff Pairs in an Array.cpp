f911b7edd7065347a578f0dfea91e04b435233c2
suraj kumar oraon <surajcroaxx00097@gmail.com>
2020-10-03 14:04:08 +0530

    Create K-diff Pairs in an Array.cpp

---
+++ October LeetCoding Challenge/Week 1 October 1st - October 7th/K-diff Pairs in an Array.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int findPairs(vector<int>& nums, int k) {
+        int sol=0;
+        unordered_map<int,int> s;
+        unordered_map<int,int> m;
+        int a,b;
+        for(int i=0;i<nums.size();i++)
+            m[nums[i]]=i;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(m.count(nums[i]-k)>0 && m[nums[i]-k]!=i)
+            {
+                a=nums[i]-k;
+                b=nums[i];
+                s[a]=b;
+            }
+            if(m.count(nums[i]+k)>0 && m[nums[i]+k]!=i)
+            {
+                a=nums[i];
+                b=nums[i]+k;
+                s[a]=b;
+            }
+        }
+        return s.size();
+    }
+};
