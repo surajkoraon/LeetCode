62a3cf87cfacf61ab0473c85a27e9223999aafa7
suraj <surajcroaxx00097@gmail.com>
2020-11-01 00:36:06 +0530

    added files

---
+++ October LeetCoding Challenge/Week 5: October 29th - October 31st/Recover Binary Search Tree.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void helper(TreeNode*& root,TreeNode*& first,TreeNode*& second,TreeNode*& prev)
+    {
+        if(!root)
+            return;
+        helper(root->left,first,second,prev);
+        if(prev)
+        {
+            if(root->val<prev->val)
+            {
+                if(!first)
+                    first=prev;
+                second=root;
+            }
+        }
+        prev=root;
+        helper(root->right,first,second,prev);
+    }
+    void recoverTree(TreeNode* root) {
+        TreeNode *first=NULL,*second=NULL,*prev=NULL;
+        helper(root,first,second,prev);
+        swap(first->val,second->val);
+    }
+};
