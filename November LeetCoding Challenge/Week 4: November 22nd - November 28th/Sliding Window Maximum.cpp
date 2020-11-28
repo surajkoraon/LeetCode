0b8e930f0ac38cdc6b70afd12cb6dc02b17fce89
suraj <surajcroaxx00097@gmail.com>
2020-11-28 18:36:46 +0530

    added file

---
+++ November LeetCoding Challenge/Week 4: November 22nd - November 28th/Sliding Window Maximum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> v;
+        int n=nums.size();
+        deque<int> q;
+        for(int i=0;i<n;i++)
+        {
+            while(!q.empty() && q.front()<=i-k)
+            {
+                q.pop_front();
+            }
+            while(!q.empty() && nums[q.back()]<=nums[i])
+            {
+                q.pop_back();
+            }
+            q.push_back(i);
+            if(i>=k-1)
+                v.push_back(nums[q.front()]);
+        }
+        return v;
+    }
+};
+
