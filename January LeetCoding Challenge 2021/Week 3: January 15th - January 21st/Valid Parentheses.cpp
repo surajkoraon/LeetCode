9df9a5ced10339464b9551110d92137a49cb62e1
suraj <surajcroaxx00097@gmail.com>
2021-01-20 23:07:03 +0530

    added file

---
+++ January LeetCoding Challenge 2021/Week 3: January 15th - January 21st/Valid Parentheses.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isValid(string s) {
+      stack<char> stk;
+      for(char ch:s)
+      {
+         if(ch=='(' || ch=='{' || ch=='[')
+             stk.push(ch);
+         else if(!stk.empty() && ((ch==')' && stk.top()=='(') || (ch=='}' && stk.top()=='{') || (ch==']' && stk.top()=='[')))
+             stk.pop();
+         else
+             return false;
+      }
+      return stk.empty();   
+    }
+};
