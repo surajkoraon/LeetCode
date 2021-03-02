7348e417ace1c9509454372788bdf5c4c95f6ecb
surajkoraon <surajcroaxx00097@gmail.com>
2021-03-02 20:28:38 +0530

    added file

--- March LeetCoding Challenge 2021/Week 1: March 1st - March 7th/Set Mismatch.cpp
+++ March LeetCoding Challenge 2021/Week 1: March 1st - March 7th/Set Mismatch.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-        
+        int n=nums.size();
+        vector<bool> v(n+1,false);
+        int a,b,sum=0;
+        for(int x:nums)
+        {
+            if(!v[x])
+            {
+                v[x]=true;
+            }
+            else
+            {
+                a=x;
+            }
+            sum+=x;
+        }
+        n=n*(n+1)/2;
+        b=n-sum+a;
+        return {a,b};
     }
 };
