b9a2471a9ba7ed0efa04e01e9399790739e5776b
surajkoraon <surajcroaxx00097@gmail.com>
2021-03-08 22:26:09 +0530

    small optimization

--- March LeetCoding Challenge 2021/Week 2: March 8th - March 14th/Remove Palindromic Subsequences.cpp
+++ March LeetCoding Challenge 2021/Week 2: March 8th - March 14th/Remove Palindromic Subsequences.cpp
@@ -4,7 +4,7 @@ public:
         int n=s.length();
         if(n==0)
             return 0;
-        for(int i=0;i<n;i++)
+        for(int i=0;i<n/2;i++)
         {
             if(s[i]!=s[n-i-1])
                 return 2;
