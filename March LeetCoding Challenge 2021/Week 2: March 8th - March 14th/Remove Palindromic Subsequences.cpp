f31eac320b1b2fb299b9702c8bd7a86deec73c94
surajkoraon <surajcroaxx00097@gmail.com>
2021-03-08 22:23:46 +0530

    added file

---
+++ March LeetCoding Challenge 2021/Week 2: March 8th - March 14th/Remove Palindromic Subsequences.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int removePalindromeSub(string s) {
+        int n=s.length();
+        if(n==0)
+            return 0;
+        for(int i=0;i<n;i++)
+        {
+            if(s[i]!=s[n-i-1])
+                return 2;
+        }
+        return 1;
+    }
+};
