0b7c910ad52596b1d8762bfbe15aa2c6e4c50807
surajkoraon <surajcroaxx00097@gmail.com>
2021-05-03 01:23:10 +0530

    added file

---
+++ April LeetCoding Challenge 2021/Week 3: April 15th - April 21st/Combination Sum IV.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<unsigned int> dp(target+1,0);
+        dp[0]=1;
+        for(int j=1;j<=target;j++)
+        {
+            for(int i=0;i<nums.size();i++)
+            {
+                if(j>=nums[i])
+                    dp[j]+=dp[j-nums[i]];
+            }
+        }
+        return dp[target];
+    }
+};
